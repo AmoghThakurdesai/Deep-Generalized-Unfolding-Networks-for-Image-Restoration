3d16fbd56234cb5e2782bfd5726b7fd23880644d
AmoghThakurdesai <105233685+AmoghThakurdesai@users.noreply.github.com>
2024-02-27 12:24:51 +0530

    Update dataset_RGB.py

--- Deraining/dataset_RGB.py
+++ Deraining/dataset_RGB.py
@@ -13,11 +13,11 @@ class DataLoaderTrain(Dataset):
     def __init__(self, rgb_dir, img_options=None):
         super(DataLoaderTrain, self).__init__()
 
-        inp_files = sorted(os.listdir(os.path.join(rgb_dir, 'input')))
-        tar_files = sorted(os.listdir(os.path.join(rgb_dir, 'target')))
+        inp_files = sorted(os.listdir(os.path.join(rgb_dir, 'rainy')))
+        tar_files = sorted(os.listdir(os.path.join(rgb_dir, 'd rainy')))
 
-        self.inp_filenames = [os.path.join(rgb_dir, 'input', x)  for x in inp_files if is_image_file(x)]
-        self.tar_filenames = [os.path.join(rgb_dir, 'target', x) for x in tar_files if is_image_file(x)]
+        self.inp_filenames = [os.path.join(rgb_dir, 'rainy', x)  for x in inp_files if is_image_file(x)]
+        self.tar_filenames = [os.path.join(rgb_dir, 'd rainy', x) for x in tar_files if is_image_file(x)]
 
         self.img_options = img_options
         self.sizex       = len(self.tar_filenames)  # get the size of target
